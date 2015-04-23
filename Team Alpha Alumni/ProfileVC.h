c8ab9d508f5c18c8a7cd244b5fc51f49fa3d51aa
mzhang0 <jbravo@JBravos-Mac.local>
2015-04-23 11:33:51 -0400

    Fixed collection layout

--- Team Alpha Alumni/ProfileVC.h
+++ Team Alpha Alumni/ProfileVC.h
@@ -9,4 +9,20 @@
 
 @interface ProfileVC : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *NameLabel;
+
+@property (weak, nonatomic) IBOutlet UIImageView *ProfileImage;
+
+@property (weak, nonatomic) IBOutlet UILabel *WorkLabel;
+
+@property (weak, nonatomic) IBOutlet UITextView *RoleText;
+
+@property (weak, nonatomic) IBOutlet UITextView *StartingYearText;
+
+@property (weak, nonatomic) IBOutlet UITextView *LocationText;
+
+@property (weak, nonatomic) IBOutlet UITextView *FavoriteMemoryText;
+
+@property (weak, nonatomic) IBOutlet UITextView *ExperienceText;
+
 @end
